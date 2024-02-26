3432399dd81131e2746630de239f26288506bafa
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-02-26 21:34:02 +0000

    finish the find_neighbors method in WordDictionary

--- Word Ladder Game/word_dictionary.py
+++ Word Ladder Game/word_dictionary.py
@@ -18,9 +18,7 @@ e.g.
        /
       *e
 
-Searching for a word: O(l), l = length of the word
 '''
-
 class TrieNode:
     def __init__(self):
         self.children = {}
@@ -33,7 +31,56 @@ class WordDictionary:
         self.__file_input()
 
     def find_neighbors(self, word):
-        pass
+        if not word:
+            return []
+        
+        original_word = word
+        n = len(word)
+        res = set()
+        # Create permutations of the word
+        # e.g. 'cow' -> ['*ow', 'c*w', 'co*'], 
+        # (* indicates any alphabets)
+        permutations = [word[:i] + '*' + word[i + 1:] for i in range(n)]
+
+        cache = []
+        def dfs(i, node):
+            # Enable dfs() to access the outer variable res
+            nonlocal res
+
+            # 1. Check i == n (whether the search reaches the end of the word)
+            # 2. Check node.end_of_word (whether the word exists in the Trie)
+            # 3. Add the found word to res
+            if i == n and node.end_of_word:
+                res.add(''.join(cache))
+                return
+            
+            # Stop the recursion if ...
+            # 1. The search reaches the end of the word but no valid words are found
+            # 2. The character is not a child of the node nor is it a '*'
+            if (i == n or
+                word[i] != '*' and 
+                word[i] not in node.children):
+                return 
+        
+            for child in node.children:
+                # Continue the recursion only on the subtree of the current character
+                # or all children if the character is a '*'
+                if word[i] == '*' or word[i] == child:
+                    # Backtracking 
+                    # Using a cache variable to store the combinations of the word
+                    # Add char -> dfs -> remove char when dfs returns
+                    # Instead of passing combination as parameter, dfs(i, node, combination)
+                    # Reusing the variable to reduce memory usage 
+                    cache.append(child)
+                    # Move pointer i to the next character and set the child node to be the new root
+                    dfs(i + 1, node.children[child])
+                    cache.pop()
+        
+        for word in permutations:
+            dfs(0, self.root)
+
+        res.discard(original_word)
+        return list(res)
 
     def __file_input(self):
         with open('words_alpha.txt', 'r') as f:
@@ -41,14 +88,27 @@ class WordDictionary:
                 self.__insert(word)
 
     def __insert(self, word):
+        # Create a pointer to the root
         node = self.root
 
+        # Check if the character is a child of the root
         for char in word:
+            # Create a new branch if the character is not found
             if char not in node.children:
                 node.children[char] = TrieNode()
+            # Traverse to the node storing the character 
             node = node.children[char]
             
+        # Set the node storing the last character of the word to be the end_of_word
         node.end_of_word = True
+
+# Testing
+if __name__ == '__main__':
+    wd = WordDictionary()
+    print(wd.find_neighbors('!'))
+    print(wd.find_neighbors('cow'))
+    print(wd.find_neighbors('life'))
+
     
         
 
