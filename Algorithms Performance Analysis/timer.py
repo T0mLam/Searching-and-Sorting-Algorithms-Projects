320fda768bf620cbd144c2fe5d9c1a3d8a0bcd28
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-02-27 10:15:30 +0000

    Delete Algorithms Performance Analysis directory

--- Algorithms Performance Analysis/timer.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def timer(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        func(*args, **kwargs)
-        end_time = time.perf_counter()
-        return end_time - start_time
-    return wrapper
