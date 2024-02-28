144bbd776569cdb9e24c626e5a04d34ac04be596
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-02-28 22:35:44 +0000

    update .gitignore

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from timer import timer
 
 
 class WordLadder:
+    @timer
     def __init__(self):
         self.word_dict = WordDictionary() 
 
+    @timer
     def find_shortest_path(self, src, dst):
         # Find the shortest path from the source to the destination using BFS
         pass
 
+    @timer
     def find_k_shortest_paths(self, src, dst, k):
         # Optional
         pass
