73d2faa48f4a3ca57cde8286d5abee8fbdcb1153
Oluwafemi okono <148986193+fokono18@users.noreply.github.com>
2024-02-29 13:57:31 +0000

    k_shortest_function

--- main.py
+++ main.py
@@ -24,10 +24,32 @@ class WordLadder:
         return []
 
     @timer
-    def find_k_shortest_paths(self, src, dst, k):
-        # Optional
-        pass
-
+     def find_k_shortest_paths(self, src, dst, k):
+       if src == dst:
+           return [[src]]
+          
+       shortest_path = self.find_shortest_path(src, dst)
+       if not shortest_path:
+           return []
+       
+       k_shortest_paths = [shortest_path]
+       
+       visited = set(shortest_path)
+       
+       queue = deque([(len(shortest_path), shortest_path)])
+       while queue and len(k_shortest_paths) < k:
+           _, path = queue.popleft()
+           for i in range(len(path)):
+               for neighbor in self.word_dictionary.get_neighbors(path[i]):
+                   if neighbor not in visited:
+                       new_path = path[:i] + [neighbor] + path[i+1:]
+                       if new_path not in k_shortest_paths:
+                           k_shortest_paths.append(new_path)
+                           visited.add(neighbor)
+                           queue.append((len(new_path), new_path))
+                           
+                           queue = deque(sorted(queue))
+       return k_shortest_paths
 
 if __name__ == '__main__':
     wl = WordLadder()
