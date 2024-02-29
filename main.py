f198ede23560905bc887fa82cd257a8adbf649b2
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-02-29 13:37:12 +0000

    update find_shortest_path

--- main.py
+++ main.py
@@ -12,15 +12,14 @@ class WordLadder:
     def find_shortest_path(self, src, dst):
         if src == dst:
             return src
+        
         queue = deque([(src, [src])])
-        visited = set([src])
+
         while queue:
             current_word, path = queue.popleft()
             for neighbor in self.word_dictionary.find_neighbors(current_word):
                 if neighbor == dst:
                     return path + [neighbor]
-            if neighbor  not in visited:
-                visited.add(neighbor)
                 queue.append((neighbor, path + [neighbor]))
         return []
 
@@ -32,4 +31,4 @@ class WordLadder:
 
 if __name__ == '__main__':
     wl = WordLadder()
-    print(wl.find_shortest_path('cow', 'hi'))
+    print(wl.find_shortest_path('cow', 'man'))
