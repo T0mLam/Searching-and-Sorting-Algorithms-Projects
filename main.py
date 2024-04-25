f9a31484d8da24b4c85de3a0781a2849c01a15c2
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-04-25 23:44:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from word_dictionary import WordDictionary
-from timer import timer
 from collections import deque
+from timer import timer
+
+from word_dictionary import WordDictionary
 
 
 class WordLadder:
@@ -28,36 +29,7 @@ class WordLadder:
                 visited.add(neighbor)
         return []
 
-    @timer
-    def find_k_shortest_paths(self, src, dst, k):
-       if src == dst:
-           return [[src]]
-          
-       shortest_path = self.find_shortest_path(src, dst)
-       if not shortest_path:
-           return []
-       
-       k_shortest_paths = [shortest_path]
-       
-       visited = set(shortest_path)
-       
-       queue = deque([(len(shortest_path), shortest_path)])
-       while queue and len(k_shortest_paths) < k:
-           _, path = queue.popleft()
-           for i in range(len(path)):
-               for neighbor in self.word_dictionary.find_neighbors(path[i]):
-                   if neighbor not in visited:
-                       new_path = path[:i] + [neighbor] + path[i+1:]
-                       if new_path not in k_shortest_paths:
-                           k_shortest_paths.append(new_path)
-                           visited.add(neighbor)
-                           queue.append((len(new_path), new_path))
-                           
-                           queue = deque(sorted(queue))
-       return k_shortest_paths
-
 
 if __name__ == '__main__':
     wl = WordLadder()
     print(wl.find_shortest_path('eeee', 'ffff'))
-    print(wl.find_k_shortest_paths('cow', 'man', 2))
